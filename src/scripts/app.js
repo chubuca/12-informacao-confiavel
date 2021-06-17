7e30d25c2522604d529dd1a6fc9fdd8731da13ca
Luca Regne <luca.rsr.luca@gmail.com>
2021-06-16 22:49:51 -0300

    Changes made by Davi Augusto and Priscila (Dark Mode and very pretty login/register form)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,14 +7,14 @@ var usuarios = [
   {
     nome: "admin",
     senha: "admin",
-    email: "admin@admin.com"
+    email: "admin@admin.com",
   },
   {
     nome: "teste",
     senha: "teste",
-    email: "teste@admin.com"
+    email: "teste@admin.com",
   },
-]
+];
 
 function renderizaElementos() {
   resultado_container = $("#resultado-container");
@@ -33,21 +33,21 @@ function renderizaElementos() {
 
       resultado_container.append(
         `<div class='resultado' id='${key}'>` +
-        `<img id='icon-feedback' src='' pontuacao='${reputation}'>` +
-        "<div class='texto-resultado'>" +
-        `<a href='${webpage.url}' class='dominio'>` +
-        `<h1 class='titulo'>${webpage.name}</h1>` +
-        "</a>" +
-        `<p class='not'>${webpage.description}</p>` +
-        "</div>"
+          `<img id='icon-feedback' src='' pontuacao='${reputation}'>` +
+          "<div class='texto-resultado'>" +
+          `<a href='${webpage.url}' class='dominio'>` +
+          `<h1 class='titulo'>${webpage.name}</h1>` +
+          "</a>" +
+          `<p class='not'>${webpage.description}</p>` +
+          "</div>"
       );
       resultado_container.append(
         //Favorito
         `<svg class='fav-icon' id="fav-icon-${key}" onclick='setFav("${key}")' version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.481 19.481" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 19.481 19.481">` +
-        `<g>` +
-        `<path d="m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z"/>` +
-        `</g>` +
-        `</svg>`
+          `<g>` +
+          `<path d="m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z"/>` +
+          `</g>` +
+          `</svg>`
       );
       if (webpage.fav == true) {
         let svg = $(`#fav-icon-${key}`);
@@ -57,30 +57,30 @@ function renderizaElementos() {
       resultado_container.append(
         //Comeco dos botões de avaliação
         "<div class='dropdown'>" +
-        "<button class='btn btn-secondary dropdown-toggle like' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>" +
-        "Gostei" +
-        "</button>" +
-        //DropDown Avaliações Positivas
-        "<ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>" +
-        `<li><a onClick='changeReputation("${key}", 1)' class='dropdown-item' href='#'>Design agradável</a></li>` +
-        `<li><a onClick='changeReputation("${key}", 2)' class='dropdown-item' href='#'>Informativo</a></li>` +
-        `<li><a onClick='changeReputation("${key}", 3)' class='dropdown-item' href='#'>Interface Intuitiva</a></li>` +
-        `<li><a onClick='changeReputation("${key}", 4)' class='dropdown-item' href='#'>Imparcialidade</a></li>` +
-        "</ul>" +
-        "</div>" +
-        //DropDown Avaliações Negativas
-        "<div class='dropdown'>" +
-        "<button class='btn btn-secondary dropdown-toggle dislike' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>" +
-        "Não gostei" +
-        "</button>" +
-        "<ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>" +
-        `<li><a onClick='changeReputation("${key}", -1)' class='dropdown-item' href='#'>Poluição visual</a></li>` +
-        `<li><a onClick='changeReputation("${key}", -2)' class='dropdown-item' href='#'>Informações falsas</a></li>` +
-        `<li><a onClick='changeReputation("${key}", -3)' class='dropdown-item' href='#'>Interface confusa</a></li>` +
-        `<li><a onClick='changeReputation("${key}", -4)' class='dropdown-item' href='#'>Conteúdo muito parcial</a></li>` +
-        "</ul>" +
-        "</div>" +
-        "</div>"
+          "<button class='btn btn-secondary dropdown-toggle like' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>" +
+          "Gostei" +
+          "</button>" +
+          //DropDown Avaliações Positivas
+          "<ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>" +
+          `<li><a onClick='changeReputation("${key}", 1)' class='dropdown-item' href='#'>Design agradável</a></li>` +
+          `<li><a onClick='changeReputation("${key}", 2)' class='dropdown-item' href='#'>Informativo</a></li>` +
+          `<li><a onClick='changeReputation("${key}", 3)' class='dropdown-item' href='#'>Interface Intuitiva</a></li>` +
+          `<li><a onClick='changeReputation("${key}", 4)' class='dropdown-item' href='#'>Imparcialidade</a></li>` +
+          "</ul>" +
+          "</div>" +
+          //DropDown Avaliações Negativas
+          "<div class='dropdown'>" +
+          "<button class='btn btn-secondary dropdown-toggle dislike' type='button' id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>" +
+          "Não gostei" +
+          "</button>" +
+          "<ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>" +
+          `<li><a onClick='changeReputation("${key}", -1)' class='dropdown-item' href='#'>Poluição visual</a></li>` +
+          `<li><a onClick='changeReputation("${key}", -2)' class='dropdown-item' href='#'>Informações falsas</a></li>` +
+          `<li><a onClick='changeReputation("${key}", -3)' class='dropdown-item' href='#'>Interface confusa</a></li>` +
+          `<li><a onClick='changeReputation("${key}", -4)' class='dropdown-item' href='#'>Conteúdo muito parcial</a></li>` +
+          "</ul>" +
+          "</div>" +
+          "</div>"
       );
       let element = "#" + key + " #icon-feedback";
 
@@ -178,11 +178,10 @@ function filtroBaixo() {
         webpage.reputation.ui +
         webpage.reputation.partiality;
       if (reputation <= 0) {
-        $(`#${key}`).css("display", 'none');
+        $(`#${key}`).css("display", "none");
       }
     }
   }
-
 }
 
 function setFav(id) {
@@ -260,24 +259,44 @@ function showFavorites() {
           <h4>Share</h4>
           <div class="social">
               <a 
-                href="www.facebook.com/sharer/sharer.php?u=${window.location.href}&title=${webpage.name}%20tem%20nota%20${reputation}%20de%20confiabilididade" 
+                href="www.facebook.com/sharer/sharer.php?u=${
+                  window.location.href
+                }&title=${
+          webpage.name
+        }%20tem%20nota%20${reputation}%20de%20confiabilididade" 
                 id="share-fb" class="sharer button"><i class="fa fa-2x fa-facebook-square"></i>
               </a>
               <a 
-                href="http://twitter.com/intent/tweet?original_referer=${window.location.href}&text=${webpage.name}%20tem%20nota%20${reputation}%20de%20confiabilididade&url=${window.location.href}" 
+                href="http://twitter.com/intent/tweet?original_referer=${
+                  window.location.href
+                }&text=${
+          webpage.name
+        }%20tem%20nota%20${reputation}%20de%20confiabilididade&url=${
+          window.location.href
+        }" 
                 id="share-tw" 
                 class="sharer button">
                   <i class="fa fa-2x fa-twitter-square"></i>
                 </a>
               <a 
-                href="http://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}&&title=${webpage.name}%20tem%20nota%20${reputation}%20de%20confiabilididade&source=${$(location).attr('hostname')}&"
+                href="http://www.linkedin.com/shareArticle?mini=true&url=${
+                  window.location.href
+                }&&title=${
+          webpage.name
+        }%20tem%20nota%20${reputation}%20de%20confiabilididade&source=${$(
+          location
+        ).attr("hostname")}&"
                 id="share-li"
                 class="sharer
                 button">
                   <i class="fa fa-2x fa-linkedin-square"></i>
               </a>
               <a 
-                href="mailto:?subject=Reputacao%20do%20site%20${webpage.name}&body=${webpage.name}%20tem%20nota%20${reputation}%20de%20confiabilididade"
+                href="mailto:?subject=Reputacao%20do%20site%20${
+                  webpage.name
+                }&body=${
+          webpage.name
+        }%20tem%20nota%20${reputation}%20de%20confiabilididade"
                 id="share-em"
                 class="sharer button">
                   <i class="fa fa-2x fa-envelope-square"></i>
@@ -298,17 +317,16 @@ function showFavorites() {
     }
   }
 }
+
 function cadastro() {
-  let nome = $('#username').val();
-  let senha = $('#password').val();
-  let confirma = $('#password-confirm').val();
-  let email = $('#email').val();
+  let nome = $("#username").val();
+  let senha = $("#password").val();
+  let confirma = $("#password-confirm").val();
+  let email = $("#email").val();
   let user = { nome: nome, senha: senha, email: email };
-  if (nome === '' || senha === '' || email === '') 
-    alert("Erro no cadastro");
-  else if (senha != confirma)
-    alert("Senha inserida incorretamente");
-  else{
+  if (nome === "" || senha === "" || email === "") alert("Erro no cadastro");
+  else if (senha != confirma) alert("Senha inserida incorretamente");
+  else {
     usuarios = [...usuarios, user];
     alert("Cadastrado corretamente.");
     window.location.href = "index.html";
@@ -320,7 +338,10 @@ function login() {
   let senha = document.getElementById("password").value;
   let teste = false;
   for (i = 0; i < usuarios.length; i++) {
-    if ((nome == usuarios[i].nome || nome == usuarios[i].email) && senha == usuarios[i].senha) {
+    if (
+      (nome == usuarios[i].nome || nome == usuarios[i].email) &&
+      senha == usuarios[i].senha
+    ) {
       localStorage.setItem("userlogin", true);
       alert("Login efetuado com sucesso!");
       window.location.href = "index.html";
@@ -333,33 +354,194 @@ function login() {
   }
 }
 
-/*
-for( i = 0; i < localStorage.length ; i++){
-    if((nome == localStorage[i].nome || nome == localStorage[i].email) && senha == localStorage[i].senha){
-      localStorage.setItem("userlogin",true);
-      alert("Login efetuado com sucesso!");
-      window.location.href = "index.html" ;
-      teste = true;
-      return;
-    } 
+class Validator {
+  constructor() {
+    this.validations = [
+      "data-min-length",
+      "data-max-length",
+      "data-only-letters",
+      "data-email-validate",
+      "data-required",
+      "data-equal",
+      "data-password-validate",
+    ];
   }
-  if (teste == false) {
-    alert("Inválido");
+
+  // inicia a validação de todos os campos
+  validate(form) {
+    // limpa todas as validações antigas
+    let currentValidations = document.querySelectorAll(
+      "form .error-validation"
+    );
+
+    if (currentValidations.length) {
+      this.cleanValidations(currentValidations);
+    }
+
+    // pegar todos inputs
+    let inputs = form.getElementsByTagName("input");
+    // transformar HTMLCollection em arr
+    let inputsArray = [...inputs];
+
+    // loop nos inputs e validação mediante aos atributos encontrados
+    inputsArray.forEach(function (input, obj) {
+      // fazer validação de acordo com o atributo do input
+      for (let i = 0; this.validations.length > i; i++) {
+        if (input.getAttribute(this.validations[i]) != null) {
+          // limpa string para saber o método
+          let method = this.validations[i]
+            .replace("data-", "")
+            .replace("-", "");
+
+          // valor do input
+          let value = input.getAttribute(this.validations[i]);
+
+          // invoca o método
+          this[method](input, value);
+        }
+      }
+    }, this);
+  }
+
+  // método para validar se tem um mínimo de caracteres
+  minlength(input, minValue) {
+    let inputLength = input.value.length;
+
+    let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
+    if (inputLength < minValue) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // método para validar se passou do máximo de caracteres
+  maxlength(input, maxValue) {
+    let inputLength = input.value.length;
+
+    let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
+
+    if (inputLength > maxValue) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // método para validar strings que só contem letras
+  onlyletters(input) {
+    let re = /^[A-Za-z]+$/;
+
+    let inputValue = input.value;
+
+    let errorMessage = `Este campo não aceita números nem caracteres especiais`;
+
+    if (!re.test(inputValue)) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // método para validar e-mail
+  emailvalidate(input) {
+    let re = /\S+@\S+\.\S+/;
+
+    let email = input.value;
+
+    let errorMessage = `Insira um e-mail no padrão matheus@email.com`;
+
+    if (!re.test(email)) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // verificar se um campo está igual o outro
+  equal(input, inputName) {
+    let inputToCompare = document.getElementsByName(inputName)[0];
+
+    let errorMessage = `Este campo precisa estar igual ao ${inputName}`;
+
+    if (input.value != inputToCompare.value) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // método para exibir inputs que são necessários
+  required(input) {
+    let inputValue = input.value;
+
+    if (inputValue === "") {
+      let errorMessage = `Este campo é obrigatório`;
+
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // validando o campo de senha
+  passwordvalidate(input) {
+    // explodir string em array
+    let charArr = input.value.split("");
+
+    let uppercases = 0;
+    let numbers = 0;
+
+    for (let i = 0; charArr.length > i; i++) {
+      if (
+        charArr[i] === charArr[i].toUpperCase() &&
+        isNaN(parseInt(charArr[i]))
+      ) {
+        uppercases++;
+      } else if (!isNaN(parseInt(charArr[i]))) {
+        numbers++;
+      }
+    }
+
+    if (uppercases === 0 || numbers === 0) {
+      let errorMessage = `A senha precisa um caractere maiúsculo e um número`;
+
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  // método para imprimir mensagens de erro
+  printMessage(input, msg) {
+    // checa os erros presentes no input
+    let errorsQty = input.parentNode.querySelector(".error-validation");
+
+    // imprimir erro só se não tiver erros
+    if (errorsQty === null) {
+      let template = document
+        .querySelector(".error-validation")
+        .cloneNode(true);
+
+      template.textContent = msg;
+
+      let inputParent = input.parentNode;
+
+      template.classList.remove("template");
+
+      inputParent.appendChild(template);
+    }
+  }
+
+  // remove todas as validações para fazer a checagem novamente
+  cleanValidations(validations) {
+    validations.forEach((el) => el.remove());
   }
 }
-*/
 
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit");
+
+let validator = new Validator();
+
+// evento de envio do form, que valida os inputs
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  validator.validate(form);
+});
 
 if (localStorage.length === 0) {
   $.getJSON("db/db.json", function (data) {
     data.forEach((webpage, index) => {
       localStorage.setItem(`webpage_${index}`, JSON.stringify(webpage));
     });
-
   });
-  // $.getJSON("db/user.json", function (data) {
-  //   data.forEach((user, index) => {
-  //     localStorage.setItem(`user_${index}`, JSON.stringify(user));
-  //   });
-  // })
 }
